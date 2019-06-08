684775bb50253b37e67f239f48173a49c2f54621
David Krapka <[krapka.david@icloud.com]>
2019-06-08 15:36:20 +0200

    Migration to Gulp 4;

--- gulpfile.js
+++ gulpfile.js
@@ -1,125 +1,150 @@
-const gulp = require("gulp")
-const runSequence = require("run-sequence")
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
 
 
-// Browser-sync
-const browserSync = require("browser-sync").create()
+// BROWSERSYNC LIVE SERVER
+const browserSync = require('browser-sync').create()
 
-gulp.task("browserSync", () => {
+const reload = done => {
+   browserSync.reload();
+   done();
+}
 
+const liveServer = done => {
    browserSync.init({
       server: {
-         baseDir: "dev"
+         baseDir: 'dev'
       },
-      loglevel: "debug"
-   })
+      loglevel: 'debug'
+   });
+   done();
+}
 
-})
+
+// CLEAN DIST FOLDER
+const clean = require('gulp-clean')
+
+const cleanDist = () => {
+   return gulp
+      .src('dist/*', { 
+         read: false
+      })
+      .pipe(clean())
+}
 
 
-// Sass compiling
-const sass = require("gulp-sass")
+// COMPILE SASS
+const sass = require('gulp-sass')
 
-gulp.task("sass", () => {
-
-   return gulp.src("dev/sass/**/*.+(scss|sass)")
+const compileSass = () => {
+   return gulp
+      .src('dev/sass/*.+(scss|sass)')
       .pipe(sass())
-      .pipe(gulp.dest("dev/css/"))
-      // Enable Browser-sync
+      .pipe(gulp.dest('dev/css/'))
       .pipe(browserSync.reload({
          stream: true
       }))
+}
 
-})
 
-
-// Optimize images
-// Image caching
+// COMPRESS IMAGES AND CACHE THEM
 const cache = require("gulp-cache")
-
 const imagemin = require("gulp-imagemin")
 const imageminMozjpeg = require('imagemin-mozjpeg')
 
-gulp.task("images", () => {
-  
-   return gulp.src("dev/**/*.+(png|jpg|gif|svg|webp)")
+const images = () => {
+   return gulp
+      .src("dev/**/*.+(png|jpg|gif|svg|webp)")
       .pipe(cache(imagemin(
-         [imageminMozjpeg()],
-         {verbose: true}
+         [
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+               plugins: [{
+                  removeViewBox: false,
+                  collapseGroups: true
+               }]
+            }),
+            imageminMozjpeg({ progressive: true })
+         ],
+         { verbose: true }
       )))
-      .pipe(gulp.dest("dist/"))
-
-})
-
-
-// Fonts
-gulp.task('fonts', () => {
-   return gulp.src("dev/fonts/**/*")
-      .pipe(gulp.dest("dist/fonts/"))
-})
+      .pipe(gulp.dest("dist/"));
+}
 
 
-// Other files just for put through
-gulp.task("others", () => {
-   return gulp.src([
-      "dev/*.+(xml|webmanifest|txt)",
-      "dev/.htaccess"
-   ])
-      .pipe(gulp.dest("dist/"))
-})
+// PUSH FONTS THROUGH
+const fonts = () => {
+   return gulp
+      .src('dev/fonts/**/*')
+      .pipe(gulp.dest('dist/fonts/'))
+}
 
 
-// Bundling CSS and JS files (minify, babel, autoprefixer)
-const useref = require("gulp-useref")
-const gulpIf = require("gulp-if")
-const uglify = require("gulp-uglify")
-const cleanCss = require("gulp-clean-css")
-const autoprefixer = require("gulp-autoprefixer")
-const babel = require("gulp-babel")
-const htmlmin = require("gulp-htmlmin")
-const lazyPipe = require("lazypipe")
+// PUSH OTHER FILES THROUGH
+const otherFiles = () => {
+   return gulp
+      .src(
+         'dev/*.+(xml|webmanifest|txt)',
+         'dev/.htaccess'
+      )
+}
 
-gulp.task("useref", () => {
 
-  return gulp.src("dev/*.html")
+// BUNDLE, MINIMALIZE, USE BABEL, USE AUTOPREFIXER AND CLEAN HTML
+const useref = require('gulp-useref')
+const lazyPipe = require('lazypipe')
+const gulpIf = require('gulp-if')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
+const htmlMin = require("gulp-htmlmin")
+
+const bundleAndProcess = () => {
+   return gulp
+      .src('dev/*.html')
       .pipe(useref())
-      .pipe(gulpIf("*.js", lazyPipe().pipe(babel, {presets: ["env"]}).pipe(uglify)()))
-      .pipe(gulpIf("*.css", lazyPipe().pipe(autoprefixer).pipe(cleanCss)()))
-      .pipe(gulpIf("*.html", htmlmin({ collapseWhitespace: true, ignoreCustomComments: [/^!/], minifyJS: true })))
-      .pipe(gulp.dest("dist/"))
-
-})
-
-
-// Delete [dist] before new build
-const del = require("del")
-
-gulp.task("clean:dist", () => {
-   return del.sync("dist")
-})
-
-
-// ---------------------------
-
-// Local dev server
-gulp.task("watch", ["browserSync", "sass"], () => {
-   gulp.watch("dev/sass/**/*.+(scss|sass)", ["sass"]);
-   gulp.watch("dev/*.html", browserSync.reload);
-   gulp.watch("dev/js/**/*.js", browserSync.reload);
-});
-
-gulp.task("default", (callback) => {
-   runSequence(["sass", "browserSync", "watch"], callback)
-})
+      .pipe(gulpIf("*.js", 
+         lazyPipe()
+            .pipe(babel, { 
+               presets: ['@babel/env']
+            })
+            .pipe(uglify)
+         ()
+      ))
+      .pipe(gulpIf('*.css', 
+         lazyPipe()
+            .pipe(autoprefixer)
+            .pipe(cleanCSS)
+         ()
+      ))
+      .pipe(gulpIf('*.html', 
+         htmlMin({ 
+            collapseWhitespace: true,
+            ignoreCustomComments: [/^!/], 
+            minifyJS: true 
+         })
+      ))
+      .pipe(gulp.dest('dist/'))
+}
 
 
-// Build script
-gulp.task("build", (callback) => {
-   runSequence("clean:dist", ["sass", "useref", "images", "fonts", "others"], callback)
-})
+// GULP BUILD TO DIST FOLDER
+exports.build = series(cleanDist, compileSass, parallel(bundleAndProcess, images, fonts, otherFiles))
 
 
-// Clear cache
-gulp.task("clear:cache", (callback) => {
+// GULP WATCH DEV FOLDER WITH LIVE SERVER
+const watch = () => {
+   gulp.watch('dev/sass/**/*.+(scss|sass)', gulp.series(compileSass, reload));
+   gulp.watch('dev/js/**/*.js', reload);
+   gulp.watch('dev/*.html', reload);
+}
+exports.default = series(compileSass, liveServer, watch)
+
+
+// CLEAR CACHE
+const clearCache = (callback) => {
    return cache.clearAll(callback)
-})
+}
+exports.cache = clearCache
